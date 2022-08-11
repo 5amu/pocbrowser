778e9d3341b3086ab0e5e20dc8fbdc71d556c383
5amu <casalinovalerio.cv@gmail.com>
2022-08-11 12:20:38 +0200

    comments

--- src/pocbrowser/runner.cr
+++ src/pocbrowser/runner.cr
@@ -3,16 +3,36 @@ require "./scrapers/dispach.cr"
 module Pocbrowser
     class Runner
         def run
+            # Singleton instance returning configurations harvested
+            # by Pocbrowser::CLI
             config    = Pocbrowser::Config.config
+
+            # Results are recorded in an Hash
+            # { "target" => { "mode1" => [], "mode2" => [] } }
             results   = Hash(String,Hash(String, Array(String))).new
+            
+            # Concurrency control
+            # channel survives between fibers
+            # waitgroup keeps track of the number of fibers that have to return
             channel   = Channel(Nil).new
             waitgroup = 0
 
+            # If user didn't specify a mode, run all modes
+            if config.modes.size == 0
+                config.modes = ALLOWED_MODES
+            end
+
+            # Iterate targets
             config.targets.each do |target|
                 results[target] = Hash(String, Array(String)).new
+
+                # Iterate modes
                 config.modes.each do |mode|
                     results[target][mode] = [] of String
                     waitgroup += 1
+
+                    # Launch a fiber
+                    # https://crystal-lang.org/reference/1.5/guides/concurrency.html
                     spawn do
                         # Iterate between scrapers based on defined modes
                         MODE_TO_SCRAPER[mode].new.parse(target) do |output|
@@ -28,6 +48,8 @@ module Pocbrowser
             waitgroup.times do
                 channel.receive
             end
+            
+            # Print output
             puts results
         end
     end
